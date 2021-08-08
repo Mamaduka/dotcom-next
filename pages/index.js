6deabeac590e6f55d8bb9cbbefbb0acdd56f4270
George Mamadashvili <georgemamadashvili@gmail.com>
2021-08-08 11:18:55 +0400

    Update about page

--- pages/index.js
+++ pages/index.js
@@ -4,38 +4,16 @@ export default function Home() {
   return (
     <Layout>
       <article className="mb-12 prose entry lg:prose-lg max-w-none">
-        <h2 className="">Hi! I’m George</h2>
+        <h2 className="">Hi there, I’m George 👋</h2>
         <p>
-          I’m a developer from Tbilisi, Georgia. I’m working from home almost a
-          decade now, even tried digital nomad thingy, but that wasn’t for me.
+          I’m a developer from Tbilisi, Georgia. I’m working from home for almost a decade now, I even tried the digital nomad thingy, but that wasn’t for me.
         </p>
-        <p>
-          I’m proficient in a handful of programming languages. During the day,
-          I’m busy building custom plugins and Gutenberg{' '}
-          <a
-            href="https://github.com/sortabrilliant/"
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            blocks
-          </a>
-          .
-        </p>
-        <p>
-          Time to time, I also{' '}
-          <a
+        <p>I like building things. I’m also an audiobook worm and a very amateur climber.</p>
+        <p>Currently, I’m <a
             href="https://profiles.wordpress.org/mamaduka"
             target="_blank"
             rel="noreferrer noopener"
-          >
-            contribute
-          </a>{' '}
-          to WordPress and its sister projects.
-        </p>
-        <p>
-          P.S. This new blog is my latest attempt to practice technical writing.
-        </p>
-        <p>P.S.S It isn’t going so well 😅</p>
+          >contributing</a> full-time to the WordPress Core Block Editor, aka Gutenberg. GoDaddy sponsors my work on this fantastic project.</p>
       </article>
     </Layout>
   );
